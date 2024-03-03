408aa001bc5301169cccec0d74cc65189cfc2e3b
Shweta-Fulzele <shweta.fulzele@thinkitive.com>
2024-03-03 06:38:49 +0530

    minor changes

---
+++ app/build/generated/source/buildConfig/androidTest/development/debug/com/base/portfolio/test/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.base.portfolio.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.base.portfolio.development.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "development";
+  // Field from product flavor: development
+  public static final String API_KEY = "live_23bxh1nVdDbgQqsIJvZ1rTWUNm5sJY3IgoYbySqByuOIaDbdNDutBmQUV3SQGuPv";
+  // Field from product flavor: development
+  public static final String BASE_URL = "https://www.themealdb.com/";
+  // Field from product flavor: development
+  public static final boolean IS_COMBINATION_ENABLED = false;
+  // Field from product flavor: development
+  public static final Boolean LOGGING_ENABLED = true;
+}
